6362e60caac1f43cbaa20170e1028b095c642e16
DanhPhamKyanon <danh.pham@kyanon.digital>
2022-08-01 21:58:11 +0700

    push to deploy

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/",async (req,res)=>{
+    try {
+        await mongoose.connect("mongodb+srv://danh:hanhphucao@clusterblog.sbxju.mongodb.net/gamelist?retryWrites=true&w=majority",
+            {
+                useNewUrlParser: true,
+            },
+        )
+        const gameListCollection = mongoose.connection.db.collection('gamelist');
+        const gameLists = await gameListCollection.find({}).toArray();
+        console.log(gameLists[1].data)
+        res.render('index',{gameLists: gameLists.length > 0 ? gameLists[gameLists.length -1].data : []});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.listen(3000);

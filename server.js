2d6d4ecdf3cc7b0bfb961fef5658de4b317b447e
DanhPhamKyanon <danh.pham@kyanon.digital>
2022-09-10 17:54:01 +0700

    fix

--- server.js
+++ server.js
@@ -8,6 +8,58 @@ app.set('view engine', 'ejs');
 
 
 
+app.get("/hga030", async (req, res) => {
+    await mongoose.connect("mongodb://localhost:27017/pet",
+        {
+            useNewUrlParser: true,
+        },
+    )
+    try {
+        res.render('hga030');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/isn99", async (req, res) => {
+    await mongoose.connect("mongodb://localhost:27017/pet",
+        {
+            useNewUrlParser: true,
+        },
+    )
+    try {
+        res.render('isn99');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/5566688", async (req, res) => {
+    await mongoose.connect("mongodb://localhost:27017/pet",
+        {
+            useNewUrlParser: true,
+        },
+    )
+    try {
+        res.render('5566688');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.get("/p88", async (req, res) => {
+    await mongoose.connect("mongodb://localhost:27017/pet",
+        {
+            useNewUrlParser: true,
+        },
+    )
+    try {
+        res.render('p88');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 app.get("/", async (req, res) => {
     await mongoose.connect("mongodb://localhost:27017/pet",
         {
